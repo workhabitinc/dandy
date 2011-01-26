79e16572201b3c12944269aac6d3160d2d3635d2
Aaron Stewart <aaron@workhabit.com>
2011-01-26 10:50:32 -0800

    updated comment to include additional fields.

--- dandy-api/src/main/java/org/workhabit/drupal/api/entity/DrupalComment.java
+++ dandy-api/src/main/java/org/workhabit/drupal/api/entity/DrupalComment.java
@@ -4,6 +4,8 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import org.workhabit.drupal.api.annotations.IdFieldName;
 
+import java.util.Date;
+
 /**
  * Copyright 2009 - WorkHabit, Inc. - acs
  * Date: Sep 24, 2010, 4:40:51 PM
@@ -22,6 +24,10 @@ public class DrupalComment implements DrupalEntity {
     private String subject;
     @DatabaseField(canBeNull = true)
     private String comment;
+    private String name;
+    private String mail;
+    private Date timestamp;
+    private boolean status;
 
     public int getNid() {
         return nid;
@@ -65,5 +71,37 @@ public class DrupalComment implements DrupalEntity {
 
     public String getId() {
         return String.valueOf(cid);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
